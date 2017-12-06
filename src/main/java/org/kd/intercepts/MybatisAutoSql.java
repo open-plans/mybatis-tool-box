fbdfa16c266b8e5aed7353aea20be09d4bae4d83
mysad <kedong529@163.com>
2017-12-07 00:34:58 +0800

    init

---
+++ src/main/java/org/kd/intercepts/MybatisAutoSql.java
@@ -0,0 +1,115 @@
+package org.kd.intercepts;
+ 
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.Properties;
+
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ParameterMapping;
+import org.apache.ibatis.plugin.Interceptor;
+import org.apache.ibatis.plugin.Intercepts;
+import org.apache.ibatis.plugin.Invocation;
+import org.apache.ibatis.plugin.Plugin;
+import org.apache.ibatis.plugin.Signature;
+import org.apache.ibatis.reflection.MetaObject;
+import org.apache.ibatis.session.Configuration;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
+import org.apache.ibatis.type.TypeHandlerRegistry;
+/**
+ * MyBatis 性能拦截器，用于输出每条 SQL 语句及其执行时间
+ */
+@Intercepts({
+        @Signature(type = Executor.class, method = "update", args = { MappedStatement.class, Object.class }),
+        @Signature(type = Executor.class, method = "query", args = { MappedStatement.class, Object.class,
+                RowBounds.class, ResultHandler.class }) })
+public class MybatisAutoSql implements Interceptor {
+ 
+    public Object intercept(Invocation invocation) throws Throwable {
+        MappedStatement mappedStatement = (MappedStatement) invocation.getArgs()[0];
+        Object parameter = null;
+        if (invocation.getArgs().length > 1) {
+            parameter = invocation.getArgs()[1];
+        }
+        String sqlId = mappedStatement.getId();
+        BoundSql boundSql = mappedStatement.getBoundSql(parameter);
+        Configuration configuration = mappedStatement.getConfiguration();
+        Object returnValue = null;
+        long start = System.currentTimeMillis();
+        String sql = getSql(configuration, boundSql, sqlId);
+        System.err.println("执行XML方法:"+sqlId);
+        System.err.println("执行的完整的sql语句-------------------mysad");
+        System.err.println(sql);
+        returnValue = invocation.proceed();
+        long end = System.currentTimeMillis();
+        long time = (end - start);
+        if (time > 1) {
+            System.err.println("执行的sql语句的时间:"+time+"ms");
+        }
+        return returnValue;
+    }
+ 
+    public static String getSql(Configuration configuration, BoundSql boundSql, String sqlId) {
+        String sql = showSql(configuration, boundSql);
+        StringBuilder str = new StringBuilder(100);
+        str.append(sql);
+        return str.toString();
+    }
+ 
+    private static String getParameterValue(Object obj) {
+        String value = null;
+        if (obj instanceof String) {
+            value = "'" + obj.toString() + "'";
+        } else if (obj instanceof Date) {
+            DateFormat formatter = DateFormat.getDateTimeInstance(DateFormat.DEFAULT, DateFormat.DEFAULT, Locale.CHINA);
+            value = "'" + formatter.format(new Date()) + "'";
+        } else {
+            if (obj != null) {
+                value = obj.toString();
+            } else {
+                value = "";
+            }
+ 
+        }
+        return value;
+    }
+ 
+    public static String showSql(Configuration configuration, BoundSql boundSql) {
+        Object parameterObject = boundSql.getParameterObject();
+        List<ParameterMapping> parameterMappings = boundSql.getParameterMappings();
+        String sql = boundSql.getSql().replaceAll("[\\s]+", " ");
+        if (parameterMappings.size() > 0 && parameterObject != null) {
+            TypeHandlerRegistry typeHandlerRegistry = configuration.getTypeHandlerRegistry();
+            if (typeHandlerRegistry.hasTypeHandler(parameterObject.getClass())) {
+                sql = sql.replaceFirst("\\?", getParameterValue(parameterObject));
+ 
+            } else {
+                MetaObject metaObject = configuration.newMetaObject(parameterObject);
+                for (ParameterMapping parameterMapping : parameterMappings) {
+                    String propertyName = parameterMapping.getProperty();
+                    if (metaObject.hasGetter(propertyName)) {
+                        Object obj = metaObject.getValue(propertyName);
+                        sql = sql.replaceFirst("\\?", getParameterValue(obj));
+                    } else if (boundSql.hasAdditionalParameter(propertyName)) {
+                        Object obj = boundSql.getAdditionalParameter(propertyName);
+                        sql = sql.replaceFirst("\\?", getParameterValue(obj));
+                    }
+                }
+            }
+        }
+        return sql;
+    }
+ 
+    public Object plugin(Object target) {
+        return Plugin.wrap(target, this);
+    }
+
+	public void setProperties(Properties arg0) {
+	}
+ 
+   
+}
